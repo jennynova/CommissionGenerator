665458db00174c6a6b05215559e8a179b6af8ff5
Jenny Nova <jennynova@Jennys-MacBook-Pro.local>
2015-09-02 18:06:38 -0700

    Initial Commit

---
+++ CommissionGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CommissionGenerator
+//
+//  Created by Jenny Nova on 9/2/15.
+//  Copyright (c) 2015 Jenny Nova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
